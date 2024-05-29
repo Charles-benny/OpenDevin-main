437f85feb8cd684c25cdbd7850a66cf741148d21
Akash J <charlesjr792@gmail.com>
2024-05-29 20:18:02 +0530

    first commit

---
+++ opendevin/config.py
@@ -0,0 +1,66 @@
+import copy
+import os
+
+import toml
+from dotenv import load_dotenv
+
+from opendevin.schema import ConfigType
+
+load_dotenv()
+
+DEFAULT_CONFIG: dict = {
+    ConfigType.LLM_API_KEY: None,
+    ConfigType.LLM_BASE_URL: None,
+    ConfigType.WORKSPACE_DIR: os.path.join(os.getcwd(), 'workspace'),
+    ConfigType.LLM_MODEL: 'gpt-3.5-turbo-1106',
+    ConfigType.SANDBOX_CONTAINER_IMAGE: 'ghcr.io/opendevin/sandbox',
+    ConfigType.RUN_AS_DEVIN: 'true',
+    ConfigType.LLM_EMBEDDING_MODEL: 'local',
+    ConfigType.LLM_DEPLOYMENT_NAME: None,
+    ConfigType.LLM_API_VERSION: None,
+    ConfigType.LLM_NUM_RETRIES: 6,
+    ConfigType.LLM_COOLDOWN_TIME: 1,
+    ConfigType.DIRECTORY_REWRITE: '',
+    ConfigType.MAX_ITERATIONS: 100,
+    # GPT-4 pricing is $10 per 1M input tokens. Since tokenization happens on LLM side,
+    # we cannot easily count number of tokens, but we can count characters.
+    # Assuming 5 characters per token, 5 million is a reasonable default limit.
+    ConfigType.MAX_CHARS: 5_000_000,
+    ConfigType.AGENT: 'MonologueAgent',
+    ConfigType.SANDBOX_TYPE: 'ssh',
+    ConfigType.DISABLE_COLOR: 'false',
+}
+
+config_str = ''
+if os.path.exists('config.toml'):
+    with open('config.toml', 'rb') as f:
+        config_str = f.read().decode('utf-8')
+
+tomlConfig = toml.loads(config_str)
+config = DEFAULT_CONFIG.copy()
+for k, v in config.items():
+    if k in os.environ:
+        config[k] = os.environ[k]
+    elif k in tomlConfig:
+        config[k] = tomlConfig[k]
+
+
+def get(key: str, required: bool = False):
+    """
+    Get a key from the environment variables or config.toml or default configs.
+    """
+    value = os.environ.get(key)
+    if not value:
+        value = config.get(key)
+    if not value and required:
+        raise KeyError(f"Please set '{key}' in `config.toml` or `.env`.")
+    return value
+
+
+def get_fe_config() -> dict:
+    """
+    Get all the frontend configuration values by performing a deep copy.
+    """
+    fe_config = copy.deepcopy(config)
+    del fe_config['LLM_API_KEY']
+    return fe_config
